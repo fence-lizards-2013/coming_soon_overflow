98c413d9956ff7897e0bfc5e3cad3f4f65910bad
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-07-22 15:08:00 -0700

    naggy comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController 
   def show
+    # TODO-JW: why set these at all rather than using current_user.(posts|votes) in the view?
     @posts = current_user.posts
     @votes = current_user.votes
   end
