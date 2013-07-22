f4e7ccabf22387fbaa8f36c8a002c55012038365
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-07-22 15:45:10 -0700

    removed useless code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController 
   def show
-    # TODO-JW: why set these at all rather than using current_user.(posts|votes) in the view?
-    @posts = current_user.posts
-    @votes = current_user.votes
   end
 end
   
